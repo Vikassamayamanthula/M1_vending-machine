876bbc749901a9fb021b987d09e014a36d49107f
Vikassamayamanthula <100985462+Vikassamayamanthula@users.noreply.github.com>
2022-03-30 21:46:51 +0530

    Add files via upload

---
+++ 3_Implementation/end.h
@@ -0,0 +1,7 @@
+void end(void){
+ system("cls");
+ printf("\t\t\tTHANK YOU FOR USING OUR SERVICES\n");
+ printf("\t\t\t\t  Visit Us At \n\n");
+ printf("\t\t\t    vendingmachine.com.\n\n");
+ printf(" Programmed by : S.Vikas \n");
+} 
