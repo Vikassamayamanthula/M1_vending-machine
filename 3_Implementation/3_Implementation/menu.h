fc79114fe55bfc1e534d32eb67e8da9ff0d46bb2
Vikassamayamanthula <100985462+Vikassamayamanthula@users.noreply.github.com>
2022-03-31 10:45:39 +0530

    Add files via upload

---
+++ 3_Implementation/3_Implementation/menu.h
@@ -0,0 +1,8 @@
+void menu(void){
+ printf("|WELCOME TO CHOCOLATES VENDING MACHINE|\n\n");
+ printf("1.The Chocolates\n");
+ printf("2.Login To Your Account\n");
+ printf("3.View Change\n");
+ printf("4.Exit Vending Machine\n");
+ printf("5.Exit Program\n\n");
+}
